4a8e904db65690319bdb29d7043db5ff93ae6b1a
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2015-02-24 10:10:35 +0100

    Updated class version in classes_def.xml Added rawId return in L1TMuonTriggerPrimitive.h

--- L1Trigger/L1TCalorimeter/python/l1tStage2InputPatternWriter_cfi.py
+++
@@ -1,7 +0,0 @@
-
-import FWCore.ParameterSet.Config as cms
-
-l1tStage2InputPatternWriter = cms.EDAnalyzer('Stage2InputPatternWriter',
-    towerToken = cms.InputTag("caloStage2Layer1Digis"),
-    filename   = cms.untracked.string("pattern.txt")
-)
